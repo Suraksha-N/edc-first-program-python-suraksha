88354f2b09500d9714a21c91a10b712c272ae97a
Suraksha-N <104952829+Suraksha-N@users.noreply.github.com>
2023-09-15 10:57:12 +0530

    Add files via upload

---
+++ name_age program.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Hello, I am BotPro ^_^")
+age = int(input("Please enter your age"))
+if(age>18):
+    print("You can access further, Welcome :)");
+    name =(input("PLease enter your name"));
+else:
+    print("Sorry, You cannot access further!");
+
+
+# In[ ]:
+
+
+
+
